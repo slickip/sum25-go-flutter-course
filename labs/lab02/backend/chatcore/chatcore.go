bec8efa3f75d2a45d04ae7329093797d896c5b29
hehe <irinapoltorakova404@gmail.com>
2025-07-02 00:52:30 +0300

    fix chatcore

--- labs/lab02/backend/chatcore/chatcore.go
+++ labs/lab02/backend/chatcore/chatcore.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"errors"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
-// Message represents a chat message
 type Message struct {
 	Sender    string
 	Recipient string
@@ -16,16 +16,16 @@ type Message struct {
 	Timestamp int64
 }
 
-// Broker handles message routing between users
 type Broker struct {
 	ctx        context.Context
-	input      chan Message            // Incoming messages
-	users      map[string]chan Message // userID -> receiving channel
-	usersMutex sync.RWMutex            // Protects users map
-	done       chan struct{}           // For shutdown
+	input      chan Message
+	users      map[string]chan Message
+	usersMutex sync.RWMutex
+	done       chan struct{}
+
+	stopped atomic.Bool
 }
 
-// NewBroker creates a new message broker
 func NewBroker(ctx context.Context) *Broker {
 	return &Broker{
 		ctx:   ctx,
@@ -35,16 +35,14 @@ func NewBroker(ctx context.Context) *Broker {
 	}
 }
 
-// Run starts the broker event loop (fan-in/fan-out pattern)
 func (b *Broker) Run() {
 	go func() {
 		defer close(b.done)
-		defer close(b.input) // Закрываем input при завершении
 
 		for {
 			select {
 			case <-b.ctx.Done():
-				// Context canceled: close all user channels
+				b.stopped.Store(true)
 				b.usersMutex.Lock()
 				for _, ch := range b.users {
 					close(ch)
@@ -66,24 +64,26 @@ func (b *Broker) Run() {
 	}()
 }
 
-// SendMessage sends a message to the broker
 func (b *Broker) SendMessage(msg Message) error {
+	if b.stopped.Load() {
+		return errors.New("broker stopped")
+	}
+
 	select {
 	case b.input <- msg:
 		return nil
 	case <-b.ctx.Done():
+		b.stopped.Store(true)
 		return errors.New("broker stopped")
 	}
 }
 
-// RegisterUser adds a user to the broker
 func (b *Broker) RegisterUser(userID string, recv chan Message) {
 	b.usersMutex.Lock()
 	defer b.usersMutex.Unlock()
 	b.users[userID] = recv
 }
 
-// UnregisterUser removes a user from the broker
 func (b *Broker) UnregisterUser(userID string) {
 	b.usersMutex.Lock()
 	defer b.usersMutex.Unlock()
@@ -93,22 +93,18 @@ func (b *Broker) UnregisterUser(userID string) {
 	}
 }
 
-// broadcastMessage sends a message to all registered users
 func (b *Broker) broadcastMessage(msg Message) {
 	b.usersMutex.RLock()
 	defer b.usersMutex.RUnlock()
 
 	for _, ch := range b.users {
-		// Use non-blocking send
 		select {
 		case ch <- msg:
 		default:
-			// If user's channel is full, drop message
 		}
 	}
 }
 
-// sendPrivateMessage sends a message to a specific user
 func (b *Broker) sendPrivateMessage(msg Message) {
 	b.usersMutex.RLock()
 	defer b.usersMutex.RUnlock()
@@ -117,7 +113,6 @@ func (b *Broker) sendPrivateMessage(msg Message) {
 		select {
 		case ch <- msg:
 		default:
-			// Drop if recipient channel is full
 		}
 	}
 }
